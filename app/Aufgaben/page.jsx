5bb68736d4dceeb5417b3cd058ea39f0c64c5401
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-12-14 00:18:48 +0200

    add client page

--- app/Aufgaben/page.jsx
+++ app/Aufgaben/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Appointment from '../componentsClient/Appointment'
+import Umsatzsteuer from '../componentsClient/Umsatzsteuer-Id prufen'
 
 
 const page = () => {
   return (
     <div>
-        <Appointment/>
+        {/* <Appointment/> */}
+        <Umsatzsteuer/>
     </div>
   )
 }

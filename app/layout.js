12c92340bffecad956481880556123b8fda0f8c2
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-12-17 19:26:48 +0200

    make main sideBar

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
       <ThemeProvider>
         <body className={`${inter.className} bg-[#F8F9FA`}>
           <div className="flex">
-            <Sidebar2/>
-            {/* <Sidebar /> */}
+            {/* <Sidebar2/> */}
+            <Sidebar />
             
             <div className="flex flex-col w-full mt-12">
               {/* <Search/> */}
